b2c68d004ada49d89bb26b5d7a18418624f6c4ff
turkey-kim <83493231+turkey-kim@users.noreply.github.com>
2022-08-10 15:39:15 +0900

    Create 2644.py
    beckjoon, #2633(dfs algorithm)

---
+++ 2644.py
@@ -0,0 +1,33 @@
+# 백준 2644번; 촌수계산 ->dfs로 풀이함.
+import sys
+sys.setrecursionlimit(10**9)
+
+n = int(sys.stdin.readline()) # node
+a,b = map(int, sys.stdin.readline().split())
+m = int(sys.stdin.readline()) # edges
+graph = [[] for _ in range(n+1)]
+visited = [0]*(n+1) # a를 기준으로 a와 b의 촌수거리를 저장. a자신은 0으로 저장. 즉, visited[a]=0 이다.
+
+
+def dfs(x,c):
+    visited[x] = c
+    for i in graph[x]:
+        if visited[i] == 0:
+            dfs(i,c+1) # 촌수거리에 +1 처리.
+    
+
+
+for i in range(m):
+    x, y = map(int, sys.stdin.readline().split())
+    graph[x].append(y)
+    graph[y].append(x)
+
+
+#x,y 사이의 거리 구하는 로직 만들어야함. 연결 안되어있으면 -1 출력.
+
+dfs(a,0) # 시작 정점 a와 촌수거리 0을 인자로 전달.
+
+if visited[b] != 0:
+    print(visited[b])
+else:
+    print(-1)
