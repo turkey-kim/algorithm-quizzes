1b94c606d0b6a77096be189c831fe133d526a988
turkey-kim <83493231+turkey-kim@users.noreply.github.com>
2022-08-25 16:16:12 +0900

    Create 1110.py
    baekjoon #1110

---
+++ 1110.py
@@ -0,0 +1,36 @@
+#baekjoon 1110
+
+import sys
+
+n = int(sys.stdin.readline())
+
+if n < 10:
+    n = [ '0', str(n) ]
+else:
+    n = list(map(str, str(n)))
+    
+arr = n
+res = arr[0]+arr[1]
+x = '10000'
+cnt = 0
+
+
+while x != res:
+    cnt += 1
+    if int(x) >= 10:
+        a, b = map(int, n)
+        x = a+b
+        x = x%10
+        n[0] = str(b)
+        n[1] =str(x)
+        x = str(b) + str(x)
+    elif 0 < int(x) < 10:
+        x = int(x)
+        n[0] = str(x)
+        n[1] =str(x)
+        x = str(x) + str(x)
+
+             
+        
+
+print(cnt)
