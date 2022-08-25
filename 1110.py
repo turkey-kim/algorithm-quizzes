31095f817a81d9058990a68b6815fbc00bccca63
turkey-kim <83493231+turkey-kim@users.noreply.github.com>
2022-08-25 16:20:00 +0900

    Update 1110.py

--- 1110.py
+++ 1110.py
@@ -28,9 +28,11 @@ while x != res:
         x = int(x)
         n[0] = str(x)
         n[1] =str(x)
-        x = str(x) + str(x)
-
-             
+        x = str(x) + str(x)           
         
 
 print(cnt)
+
+# 쓰잘대기없이 길게 풀었다. https://wook-2124.tistory.com/222
+# 숫자형을 자료형으로 불필요하게 바꾸려다보니 이렇게 됐다.
+# 내가 생각하는 풀이보다 더 괜찮은 풀이가 있나 계속 생각해봐야한다.
