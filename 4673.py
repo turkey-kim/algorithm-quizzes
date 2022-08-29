cce657c302f2aab527406db9d5885ed8b1b721b5
turkey-kim <83493231+turkey-kim@users.noreply.github.com>
2022-08-29 15:23:42 +0900

    백준 4673(부르트포스)

---
+++ 4673.py
@@ -0,0 +1,18 @@
+# 백준 4673
+
+import sys
+
+arr = []
+
+for i in range(1,10001):
+    x = list(map(str, str(i)))
+    x = sum(map(int,x))
+    i += x
+    arr.append(i)
+        
+
+for i in range(1,10001):
+    if i not in arr:
+        print(i)
+    
+
